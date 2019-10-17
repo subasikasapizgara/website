17377d1b4ebfa3ba1c3e43c90b72cf0b707f7218
subasikasapizgara <56651437+subasikasapizgara@users.noreply.github.com>
2019-10-17 16:36:11 +0300

    website

---
+++ js/whatsapp.js
@@ -0,0 +1,13 @@
+(function () {
+    var options = {
+        whatsapp: atob("KzkwICg1MzApIDAzMCAwMCA2Nw=="), // WhatsApp number
+        company_logo_url: "http://reklamfoni.com.tr/images/whatsapp-toplu-mesaj-reklamfoni.png",
+        greeting_message: "Size nasıl yardımcı olabiliriz? Hemen yazın, cevaplayalım", // Text of greeting message
+        call_to_action: "Bize yazın", // Call to action
+        position: "right", // Position may be 'right' or 'left'
+    };
+    var proto = document.location.protocol, host = "whatshelp.io", url = proto + "//static." + host;
+    var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = url + '/widget-send-button/js/init.js';
+    s.onload = function () { WhWidgetSendButton.init(host, proto, options); };
+    var x = document.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x);
+})();
