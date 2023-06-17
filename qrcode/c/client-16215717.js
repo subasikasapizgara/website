db21d17a6dca7ee06b4bafdc2d2b66ac6405e3a1
subasikasapizgara <56651437+subasikasapizgara@users.noreply.github.com>
2023-06-17 22:33:48 +0300

    Update client-16215717.js

--- qrcode/c/client-16215717.js
+++ qrcode/c/client-16215717.js
@@ -706,11 +706,11 @@ const tum_urunler = {
         },
         "tum-guvec-cesitleri": {
             id: "S-TIqLODxzE",
-            title: "Güveç Çeşitleri (Kişi Başı)",
+            title: "Güveç Çeşitleri",
             description: '<br>1 kg 4 kişilik<br>2 kg 8 kişilik<br>3 kg 12 kişilik<br>4 kg 16 kişilik<br>5 kg 20 kişilik<br>Lütfen <b>en az 4 saat</b> öncesinden rezervasyon oluşturun.',
             published: "Güveç Çeşitleri",
             cohost: "guvec-cesitleri",
-            fiyat: '200,00 ₺'
+            fiyat: '200,00 ₺ (Kişi Başı)'
         },
 
 
