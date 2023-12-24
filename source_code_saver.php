08d653523cb7461e1728285594eb2a8af77ca26e
subasikasapizgara <56651437+subasikasapizgara@users.noreply.github.com>
2023-12-24 16:58:10 +0300

    Delete source_code_saver.php

--- source_code_saver.php
+++
@@ -1,7 +0,0 @@
-<?php
-	header("Access-Control-Allow-Origin: *");
-	if (isset($_POST['html']) && isset($_POST['path']))
-	{
-		echo urldecode($_POST['path']);
-		file_put_contents(urldecode($_POST['path']).'index.html', urldecode($_POST['html']));
-	}
